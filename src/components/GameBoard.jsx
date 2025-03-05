98aaccec3d3ff49b87d8fc4b450fc0b8a6ee7671
CH8091743875984 <135175936+CH8091743875984@users.noreply.github.com>
2025-03-04 20:56:50 -0600

    Initial component structuring

---
+++ src/components/GameBoard.jsx
@@ -0,0 +1,18 @@
+import Card from "./Card";
+
+function GameBoard() {
+  return (
+    <>
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
+    </>
+  );
+}
+
+export default GameBoard;
---
+++ src/components/Scoreboard.jsx
@@ -0,0 +1,10 @@
+function Scoreboard() {
+  return (
+    <>
+      <div>Current Score</div>
+      <div>High Score</div>
+    </>
+  );
+}
+
+export default Scoreboard;
