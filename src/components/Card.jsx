c65deb356978264105adfd11e7297e279dbbbe15
CH8091743875984 <135175936+CH8091743875984@users.noreply.github.com>
2025-03-06 21:31:20 -0600

    Updated onclick to pass up id to parent component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,12 @@
 function Card({ id, countryName, countryImage, clickEvent }) {
+  const handleClickEvent = () => {
+    clickEvent(id);
+  };
+
   return (
-    <div className="cardContainer" onClick={clickEvent}>
+    <div className="cardContainer" onClick={handleClickEvent}>
       <div className="cardImage">
-        <img key={id} src={countryImage} alt="Country Flag" width="100" />
+        <img key={id} src={countryImage} alt="Country Flag" height="100" />
       </div>
       <span className="cardDescription">{countryName}</span>
     </div>
