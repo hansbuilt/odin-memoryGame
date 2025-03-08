48959dd3a9f32fbcf61e24a1b4cbf249bf3510ac
CH8091743875984 <135175936+CH8091743875984@users.noreply.github.com>
2025-03-08 11:07:22 -0600

    Added scoreboard flashing on score update, high score, reset

--- src/components/Scoreboard.jsx
+++ src/components/Scoreboard.jsx
@@ -1,9 +1,29 @@
+import { useState, useEffect } from "react";
+import "./scoreboard.css";
+
 function Scoreboard({ currentScoreValue, highScoreValue }) {
+  const [highlightClass, setHighlightClass] = useState("");
+
+  useEffect(() => {
+    if (highScoreValue !== 0) {
+      if (currentScoreValue === 0) {
+        setHighlightClass("reset-highlight");
+      } else if (currentScoreValue === highScoreValue) {
+        setHighlightClass("highscore-highlight");
+      } else {
+        setHighlightClass("increment-highlight");
+      }
+    }
+
+    const timer = setTimeout(() => setHighlightClass(""), 500);
+    return () => clearTimeout(timer);
+  }, [currentScoreValue, highScoreValue]);
+
   return (
-    <>
+    <div className={`scoreboard ${highlightClass}`}>
       <div>Current Score: {currentScoreValue}</div>
       <div>High Score: {highScoreValue}</div>
-    </>
+    </div>
   );
 }
 
